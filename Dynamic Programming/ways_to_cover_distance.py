cc1987dca0f890595d6b71e344932b6b369557b6
P4NK4J <pankajkandpal001@gmail.com>
2019-07-09 07:45:34 -0700

    recent changes

---
+++ Dynamic Programming/ways_to_cover_distance.py
@@ -0,0 +1,23 @@
+def printCountDP(dist): 
+    count = [0] * (dist + 1) 
+      
+    # Initialize base values. There is 
+    # one way to cover 0 and 1 distances 
+    # and two ways to cover 2 distance 
+    count[0] = 1
+    count[1] = 1
+    count[2] = 2
+      
+    # Fill the count array in bottom 
+    # up manner 
+    for i in range(3, dist + 1): 
+        count[i] = (count[i-1] + 
+                   count[i-2] + count[i-3]) 
+          
+    return count[dist]; 
+  
+# driver program
+print("enter the score or distance")
+dist = int(input()) 
+
+print( printCountDP(dist)) 
